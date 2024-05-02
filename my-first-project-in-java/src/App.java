c7e8f4e1718d4f196ef597409de8fd26cb382838
David Robert <54132069+davidroberrt@users.noreply.github.com>
2024-05-02 18:17:11 -0300

    Update App.java

--- my-first-project-in-java/src/App.java
+++ my-first-project-in-java/src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World! Welcome to course by DIO");
+        int ano = 2021;
+        ano = 2021;
+        final String BR = "Brasil"; // "final" não pode mais sofrer um tipo de alteração 
+        double PI = 3.14; // O valor do double é imutável, não pode ser alterado.
+        int ESTADOS_BRASILEIRO = 27; //uppercase não podemos alterar o valor
+        int ANO_2000 = 2000; // Especificando o ano após o underline, o valor pode ser mutável(exceção)
     }
 }
